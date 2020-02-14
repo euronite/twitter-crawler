017276a911569b0b1e6f03a625522d805fbc770e
Leo <2314838k@student.gla.ac.uk>
2020-02-14 13:41:10 +0000

    Start of processing tweet object and added textblob to requirements

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import tweepy
 import json
-
+from filter import *
 with open("api.json", "r") as f:
     text = json.load(f)
 auth = tweepy.OAuthHandler(text["TWITTER_APP_KEY"], text["TWITTER_APP_SECRET"])
@@ -9,10 +9,39 @@ auth.set_access_token(text["TWITTER_KEY"], text["TWITTER_SECRET"])
 
 api = tweepy.API(auth)
 
+tweet_list = []
+
+
+# class TweetObject(object):
+#     def __init__(self, status):
+#         self.description = status.user.description
+#         self.loc = status.user.location
+#         self.text = status.text
+#         self.coords = status.coordinates
+#         self.name = status.user.screen_name
+#         self.user_created = status.user.created_at
+#         self.followers = status.user.followers_count
+#         self.id_str = status.id_str
+#         self.created = status.created_at
+#         self.retweets = status.retweet_count
+#         self.bg_color = status.user.profile_background_color
+
 
 class StreamListener(tweepy.StreamListener):
     def on_status(self, status):
-        print(status.text)
+        #tweet_list.append(TweetObject(status))
+        #print(TweetObject(status))
+        description = status.user.description
+        loc = status.user.location
+        text = status.text
+        coords = status.coordinates
+        name = status.user.screen_name
+        user_created = status.user.created_at
+        followers = status.user.followers_count
+        id_str = status.id_str
+        created = status.created_at
+        retweets = status.retweet_count
+        bg_color = status.user.profile_background_color
 
     def on_error(self, status_code):
         if status_code == 420:
@@ -21,4 +50,4 @@ class StreamListener(tweepy.StreamListener):
 
 stream_listener = StreamListener()
 stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
-stream.filter(track=["trump", "clinton", "hillary clinton", "donald trump"])
+stream.filter(track=filter_list, languages=['en'])
