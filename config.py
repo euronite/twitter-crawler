85691d55a5db0653c648e6d1b594b4d2ff0d911d
Leo <2314838k@student.gla.ac.uk>
2020-02-27 18:01:20 +0000

    unique ID for tweets set

--- config.py
+++ config.py
@@ -3,8 +3,10 @@ from textblob import TextBlob
 import pymongo
 import tweepy
 
-db_client = pymongo.MongoClient("mongodb://localhost:27017/")
-db_twitter = db_client["twitter"]
+db_client = pymongo.MongoClient(
+    "mongodb://2314838k:37740025@localhost:27017/2314838kdb"
+)
+db_twitter = db_client["2314838kdb"]
 new_tweet = db_twitter["new_tweet"]
 quote_tweet = db_twitter["quote_tweet"]
 retweet = db_twitter["retweet"]
@@ -17,7 +19,7 @@ with open("api.json", "r") as f:
     text = json.load(f)
 auth = tweepy.OAuthHandler(text["TWITTER_APP_KEY"], text["TWITTER_APP_SECRET"])
 auth.set_access_token(text["TWITTER_KEY"], text["TWITTER_SECRET"])
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 
 def insert_tweet_to_db(status):
@@ -33,7 +35,9 @@ def insert_tweet_to_db(status):
     id_string = status.id_str
     tweet_created = status.created_at
     hashtags = status.entities["hashtags"]
+    user_mentions = status.entities["user_mentions"]
     retweets = status.retweet_count
+    symbols = status.entities["symbols"]
     blob = TextBlob(tweet_text)
     sent = blob.sentiment
     polarity = sent.polarity
@@ -41,19 +45,22 @@ def insert_tweet_to_db(status):
     tweet_json = {
         "description": description,
         "hashtags": hashtags,
+        "user_mentions": user_mentions,
+        "symbols": symbols,
         "name": name,
         "location": loc,
         "text": tweet_text,
         "coordinates": coords,
         "user_created": user_created,
         "followers": followers,
-        "id": id_string,
+        "_id": id_string,
         "tweet_created": tweet_created,
         "retweets": retweets,
         "sentiment_polarity": polarity,
         "subjectivity": subjectivity,
     }
-    if "RT @" in status.text:
+
+    if hasattr(status, "retweeted_status)"):
         tweet_json["retweet_user"] = status.retweeted_status.user.screen_name
         retweet.insert_one(tweet_json)
     elif status.is_quote_status:
